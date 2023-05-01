db3e17d89f8c3a25ee73899e6c15a0643c8c2d1e
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-05-01 10:01:51 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -54,10 +54,12 @@ basic.forever(function () {
         action = "Z"
     } else if (action == "D") {
         comment.comment("Insert D-Button Action Between These Comments")
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
         comment.comment("Insert D-Button Action Between These Comments")
         action = "Z"
     } else if (action == "E") {
         comment.comment("Insert E-Button Action Between These Comments")
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
         comment.comment("Insert E-Button Action Between These Comments")
         action = "Z"
     } else if (action == "F") {
